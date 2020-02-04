099a247dbfbd9ee1eefe4242f6f9a5b1ae71f069
William Dodson <wcdodson4@gmail.com>
2020-02-04 11:33:28 -0600

    exercise complete

---
+++ LinqMethodExercise/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LinqMethodExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            var videoGames = new List<string> { "FIFA", "Modern Warfare", "Wii Sports" };
+            //Method Syntax Games
+            var methodGames = videoGames.OrderBy(game => game.Length);
+            foreach (var i in methodGames)
+            {
+                Console.WriteLine(i);
+            }
+            //Query Syntax Games
+            var queryGames = from game in videoGames
+                             orderby game.Length descending
+                             select game;
+            foreach (var g in queryGames)
+            {
+                Console.WriteLine(g);
+            }
+
+            List<string> cars = new List <string> { "Maserati", "Chevrolet", "Ferrari", "Lamborghini", "Ford", "Aston Martin" };
+            //Query Syntax Cars
+            var carWithI = from car in cars
+                           where car.Contains("i")
+                           orderby car descending
+                           select car;
+            foreach (var c in carWithI)
+            {
+                Console.WriteLine(c);
+            }
+        }
+    }
+}
